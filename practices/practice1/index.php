5c521cf59c6bb9163b3cabef8cb8e674f1a4dd7f
Olya Bolshakova <soulful.olya@gmail.com>
2022-09-12 23:07:15 +0300

    added and fixed practice 1

--- practices/practice1/index.php
+++ practices/practice1/index.php
@@ -1,2 +1,22 @@
+<html lang="en">
+<head>
+<title>Hello world page</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+<h1>Таблица пользователей данного продукта</h1>
+<table>
+    <tr><th>Id</th><th>Name</th><th>Surname</th></tr>
 <?php
-echo 'Программа работает';
+$mysqli = new mysqli('mysql', 'root', '1234', 'appDB');
+$result = $mysqli->query("SELECT * FROM users");
+foreach ($result as $row){
+    echo "<tr><td>{$row['ID']}</td><td>{$row['name']}</td><td>{$row['surname']}</td></tr>";
+}
+?>
+</table>
+<?php
+phpinfo();
+?>
+</body>
+</html>
